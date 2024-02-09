fb0a8c02ae94efbbae23be399bb7194ae85044fd
Arunrock8055 <111429868+Arunrock8055@users.noreply.github.com>
2024-02-09 15:20:07 +0530

    Update prg.py

--- prg.py
+++ prg.py
@@ -1,10 +1,2 @@
 print("This is the first line")
 print("This is the second line")
-print("This is the third line")
-print("This is the fourth line")
-
-#changes
-print("new changes")
-for i in range :
-    print("hello")
-    print("hello")
